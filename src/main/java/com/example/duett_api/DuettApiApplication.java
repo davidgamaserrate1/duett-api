bbdaab67642c91fcebc09266fcf13bc5a3521735
David Gama Serrate <david.serrate@grupocard.com.br>
2024-06-29 00:18:30 -0400

    base: first commit with dependences installeds

---
+++ src/main/java/com/example/duett_api/DuettApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.duett_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuettApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuettApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/duett_api/DuettApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duett_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuettApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
