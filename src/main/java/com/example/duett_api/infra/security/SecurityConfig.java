60fad2e4442ba89060e26191ad384112e5173e12
David Gama Serrate <david.serrate@grupocard.com.br>
2024-07-03 08:41:25 -0400

    refactor: permission routes refer swagger

--- src/main/java/com/example/duett_api/infra/security/SecurityConfig.java
+++ src/main/java/com/example/duett_api/infra/security/SecurityConfig.java
@@ -32,8 +32,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api-docs").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/swagger-ui/*").permitAll()
+                        .requestMatchers("/swagger-duett-api.html", "/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
