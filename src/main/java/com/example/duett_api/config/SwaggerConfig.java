8f93d7a0857bdb1e0e2c140b3d9b42880e4ec37f
David Gama Serrate <david.serrate@grupocard.com.br>
2024-07-03 13:20:43 -0400

    feat: add tittle and description of swagger

--- src/main/java/com/example/duett_api/config/SwaggerConfig.java
+++ src/main/java/com/example/duett_api/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.duett_api.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,9 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info().title("Duett API - Gestão de usuarios")
+                        .description("Esta é uma API RESTful para gerenciamento de usuários, incluindo operações de registro, login, mudança de senha e administração de usuários.")
+                        .version("1.0.0"))
                 .components(new Components().addSecuritySchemes("bearer-jwt",
                         new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT")));
     }
